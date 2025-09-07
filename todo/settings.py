e1d35ea2fc9d44dc9717f737bdde9e83c60f9345
RupendraRaghuwanshi <roopendraraghu1c7999@gmail.com>
2025-09-07 20:32:56 +0530

    allowed host change

--- todo/settings.py
+++ todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG",default=False,cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','https://todo-django1-8.onrender.com/']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','todo-django1-8.onrender.com/']
 
 
 # Application definition
